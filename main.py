c7e9ba3685a51c5117c946e8acdedf7f592a952b
AndreVaas <vasiliev307@gmail.com>
2025-06-05 13:12:16 +0500

    added dz05/06

--- main.py
+++ main.py
@@ -6475,3 +6475,39 @@ import sqlite3
 #     data TEXT
 #     )""")
 #     cur.execute("DROP TABLE users")
+
+import sqlite3
+
+with sqlite3.connect("users.db") as con:
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    phone BLOB NOT NULL DEFAULT "+7 909 1234567",
+    age INTEGER CHECK(age > 0 AND age < 100),
+    email TEXT UNIQUE
+    )""")
+
+    # # переименование таблицы
+    # cur.execute("""
+    # ALTER TABLE users
+    # RENAME TO person_table;
+    # """)
+
+    # # добавление нового столбца
+    # cur.execute("""
+    # ALTER TABLE person_table
+    # ADD COLUMN address TEXT;
+    # """)
+
+    # # Переименование столбца
+    # cur.execute("""
+    # ALTER TABLE person_table
+    # RENAME COLUMN address TO home_address;
+    # """)
+
+    # # удаление столбца
+    cur.execute("""
+        ALTER TABLE person_table
+        DROP COLUMN home_address;
+        """)

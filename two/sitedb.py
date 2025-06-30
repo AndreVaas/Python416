1ad5dbcaad41d30253ebb5fe39ca2aebef7ee3b5
AndreVaas <vasiliev307@gmail.com>
2025-06-30 23:50:06 +0500

    added dz24/06

--- two/sitedb.py
+++ two/sitedb.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-from flask import Flask, render_template, flash, request, g
+from flask import Flask, render_template, flash, request, g, abort
 from two.fdatabase import FDataBase
 
 # конфигурация
@@ -23,8 +23,8 @@ def create_db():
     db = connect_db()
     with app.open_resource('sq_db.sql', 'r') as f:
         db.cursor().executescript(f.read())
-        db.commit()
-        db.close()
+    db.commit()
+    db.close()
 
 
 def get_db():
@@ -33,18 +33,47 @@ def get_db():
     return g.link_db
 
 
-@app.route('/')
+@app.route("/")
 def index():
     db = get_db()
     dbase = FDataBase(db)
-    return render_template('index.html', menu=dbase.get_menu())
+    return render_template('index.html', menu=dbase.get_menu(), posts=dbase.get_posts_anonce())
 
 
 @app.route("/add_post", methods=["POST", "GET"])
 def add_post():
     db = get_db()
     dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form["name"]) > 4 and len(request.form["post"]) > 10:
+            res = dbase.add_post(request.form["name"], request.form["post"])
+            if not res:
+                flash("Ошибка добавления статьи", category="error")
+            else:
+                flash("Статья добавлена успешно", category="success")
+        else:
+            flash("Ошибка добавления статьи", category="error")
+
     return render_template('add_post.html', menu=dbase.get_menu())
+
+
+@app.route("/post/<int:id_post>")
+def show_post(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.get_post(id_post)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.get_menu(), title=title, post=post)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template("page404.html", title="Страница не найдена", menu=dbase.get_menu())
 
 
 @app.teardown_appcontext
